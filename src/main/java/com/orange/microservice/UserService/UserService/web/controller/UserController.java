517a2d62504bf72560d2544e007cf27d42d8d2c8
MarvinP3 <49071645+MarvinP3@users.noreply.github.com>
2019-12-19 14:32:35 -0400

    first commit

---
+++ src/main/java/com/orange/microservice/UserService/UserService/web/controller/UserController.java
@@ -0,0 +1,7 @@
+package com.orange.microservice.UserService.UserService.web.controller;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+}
