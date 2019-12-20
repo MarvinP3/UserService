ba50602123d99a55877768e2626728f3b94a7213
MarvinP3 <49071645+MarvinP3@users.noreply.github.com>
2019-12-20 10:20:29 -0400

    Second commit

--- src/main/java/com/orange/microservice/UserService/UserService/web/controller/UserController.java
+++ src/main/java/com/orange/microservice/UserService/UserService/web/controller/UserController.java
@@ -1,7 +1,27 @@
 package com.orange.microservice.UserService.UserService.web.controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import model.Users;
 
 @RestController
 public class UserController {
+//Récupérer la liste des utilisateurs
+    @ResponseBody 
+    @RequestMapping(value="/Utilisateur", method=RequestMethod.GET)
+    public String listeUtilisateurs() {
+        return "la liste des utilisateur";
+	}	
 
+   
+
+    //Récupérer un utilisateur par son id
+    @GetMapping(value="/Utilisateurs/{id}")
+    public Users afficherUnUtilisateur(@PathVariableintid) {
+        Users utilisateur=new utilisateur(id, new String("Aspirateur"), 100 );
+        return utilisateur;
+    }
+   
 }
